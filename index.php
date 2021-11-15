b0ad6a0ff128ecd06a825f27890c929099426ed1
0gudini0 <94106548+0gudini0@users.noreply.github.com>
2021-11-15 21:31:57 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Новая верси сайта';
+$name = 'Кирюшка';
+$image = 'img/imgn.jpg';
+$email = '4208448@gmail.com';
+$phone = '+79111447926';
+
+require('components/header.php');
+require('components/about.php');
+require('components/status.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
